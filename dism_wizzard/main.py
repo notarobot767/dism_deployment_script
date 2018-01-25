0dfbee0eecc8bfb33c9f54c35ea0829180b2b2ef
Noah Ogrydziak <notarobot767@users.noreply.github.com>
2018-01-25 12:52:08 -0500

    collapse menu items and functions into same list

--- dism_wizzard/main.py
+++ dism_wizzard/main.py
@@ -1,8 +1,31 @@
 #from default_settings import DefaultSettings
 from .system_functions import SysFun
-from .menu_arrys import Menu
+from . import deploy
+from . import pwedit
+from .wpeutil import Wpeutil
+from .system_functions import SysFun
+import sys
 
 def main():
+  #menu name followed by assocaited function
+  #position 0 is name of menu
+  #the functions need to be stripped out in order for list
+  #to be passed into selectOptionArry
+  main_menu = [
+    ["Main Menu"],
+    ["Deploy Image", deploy.main],
+    ["Express Image Deployment", SysFun().wip],
+    ["Password Editor", pwedit.main],
+    ["Powershell", sys.exit],
+    ["Restart", Wpeutil().restart],
+    ["Shutdown", Wpeutil().shutdown]
+  ]
+
+  def fix(data):
+    return data[0]
+
+  fun_pos = 1
+
   while True:
     SysFun().cls()
-    Menu.main_switch[SysFun().selectOptionArry(Menu.main)]()
+    main_menu[SysFun().selectOptionArry(list(map(fix, main_menu)))][fun_pos]()
