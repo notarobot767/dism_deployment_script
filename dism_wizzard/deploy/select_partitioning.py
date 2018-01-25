868b9e65baf5d4634dad966bab54d66140a329c9
Noah Ogrydziak <notarobot767@users.noreply.github.com>
2018-01-25 12:54:51 -0500

    tab data for formatting purposes

--- dism_wizzard/deploy/select_partitioning.py
+++ dism_wizzard/deploy/select_partitioning.py
@@ -2,9 +2,9 @@ from ..system_functions import SysFun
 
 def selectPartitioning():
   partitioning = [
-  "Drive Partitioning", #menu title
-  "GDP", #1
-  "MBR"  #2
+    "Drive Partitioning", #menu title
+    "GDP", #1
+    "MBR"  #2
   ]
 
   SysFun().cls()
